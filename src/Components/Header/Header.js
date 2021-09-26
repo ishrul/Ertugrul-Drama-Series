3006fcfe2d27283c660d2f6a05dc5b2cdc89efdb
ishrul <isvskeyes360@gmail.com>
2021-09-26 13:15:58 +0600

    first commit

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Header.css";
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>Ertugrul Drama Series</h1>
+      <p>
+        This series is about the Turkish warrior Ertugrul from the 13th century,
+        one of the most famous warriors of his time and also the father of Osman
+        (the founder of the Ottoman Empire). He is an ambitious man who wants to
+        bring peace and justice to his people.
+      </p>
+    </div>
+  );
+};
+
+export default Header;
